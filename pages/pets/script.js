e4cb6bb958b74a33efcf5d78ea60fef01da1e976
sadamir <a.saduanov19@gmail.com>
2022-06-06 22:40:36 +0500

    feat: adding js functions

--- pages/pets/script.js
+++ pages/pets/script.js
@@ -11,4 +11,35 @@ window.onload=function(){
     headerList.classList.toggle("active");
     logo.classList.toggle("active");
   })
+
+
+  let http = new XMLHttpRequest();
+
+  http.open('get', 'pets.json', true);
+
+  http.send();
+
+  http.onload = function(){
+
+    if(this.readyState == 4 && this.status == 200){
+      let pets = JSON.parse(this.responseText);
+
+      let output = '';
+
+      for(let item of pets){
+        output += `
+        <div class="our-friends-card item swiper-slide">
+              <img src="${item.img}" alt="${item.img}" class="our-friends-img">
+                <div class="container-our-friends">
+                <h3 class="our-friends-name">${item.name}</h3>
+                <button type="button" onclick="" class="our-friends-learn-button">Learn more</button>
+              </div>
+            </div>
+          `;
+      }
+
+
+      document.querySelector(".our-friends-wrapper-flex1").innerHTML = output;
+    }
+  }
 }
